d4154fbe06efc1ff1ffc7e5ce22282d311325bba
Travis Moy <Travis.Moy@Staples.com>
2015-05-28 11:06:35 -0700

    Disabled linum
    It was too slow; you can see line numbers in bottom bar

--- init.el
+++ init.el
@@ -141,7 +141,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
- '(global-linum-mode t))
+ '(global-linum-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
