ec9f0a9d02a56102851f09a583e44d4dfb9341e7
Travis Moy <Travis.Moy@Staples.com>
2015-10-08 13:54:56 -0700

    Reactivating melpa

--- init.el
+++ init.el
@@ -8,8 +8,8 @@
 ;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 ;; (add-to-list 'package-archives
 ;;              '("tromey" . "http://tromey.com/elpa/") t)
-;; (add-to-list 'package-archives
-;;              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
